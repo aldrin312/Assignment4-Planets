10c71a7cbc27a2e25d58e90298b18489d06f0673
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-17 11:16:55 -0400

    created the abstract plannet class private fields

---
+++ Assignment4-Planets/Planet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Aldrin Fernandez
+ * Date: July 17, 2017
+ * Description: This is the abstract planet class
+ * Version 0.1 - created the planet class
+ */
+
+namespace Assignment4_Planets
+{
+	class Planet
+	{
+		//PRIVATE INTANCE VARIABLES
+		private double _diameter;
+		private double _mass;
+		private int _moonCount;
+		private string _name;
+		private double _orbitalPeriod;
+		private int _ringCount;
+		private double rotationalPeriod;
+		//PUBLIC PROTPERTIES
+
+		//CONSTRUCTOR----------------------------------------------------------------
+
+		//PUBLIC METHIDS
+
+		//PRIVATE METHODS
+	}
+}
--- Assignment4-Planets/Program.cs
+++ Assignment4-Planets/Program.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * Name: Aldrin Fernandez
+ * Date: July 17, 2017
+ * Description: started the project
+ * Version 0.1 - created progam.cs
+ */
+
 
 namespace Assignment4_Planets
 {
