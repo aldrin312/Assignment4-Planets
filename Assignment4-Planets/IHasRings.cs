ecee2455754fb7e16cac29337b52614ce7841cd5
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-07-18 16:30:46 -0400

    created the Terrestrial class

--- Assignment4-Planets/IHasRings.cs
+++ Assignment4-Planets/IHasRings.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * Name: Aldrin Fernandez
+ * Date: July 17, 2017
+ * Description: This is the IHasRings interface
+ * Version 0.3 - created the IHasRings interface
+ */
 
 namespace Assignment4_Planets
 {
---
+++ Assignment4-Planets/TerrestrialPlanet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Aldrin Fernandez
+ * Date: July 18, 2017
+ * Description: This is the TerrestrialPlanet class
+ * Version 0.3 - created the TerrestrialPlanet class
+ */
+
+namespace Assignment4_Planets
+{
+	class TerrestrialPlanet: Planet, IHasMoons
+	{
+		//PRIVATE INTANCE VARIABLES
+
+		private bool _oxygen;
+
+		//PUBLIC PROTPERTIES
+
+		//CONSTRUCTOR----------------------------------------------------------------
+		public TerrestrialPlanet(string name, double mass, double diameter, bool oxygen)
+			: base(name,mass,diameter)
+		{
+			this._oxygen = oxygen;
+		}
+		//PUBLIC METHODS
+		public bool HasMoons()
+		{
+			if (this.MoonCount > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public bool Habitable()
+		{
+			if (_oxygen == true)
+			{
+				
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+
+
+		//PRIVATE METHODS
+
+	}
+}
